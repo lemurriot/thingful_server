c4dce80db9bd7cece9556f41ba3e8c3fe19e27f9
Kevin Boyle <papershack@gmail.com>
2019-09-28 17:02:59 -0700

    add jwt-auth.js middleware, add verifyJwt method to auth-service

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -17,6 +17,11 @@ const AuthService = {
             aglorith: 'HS256'
         })
     },
+    verifyJwt(token){
+        return jwt.verify(token, config.JWT_SECRET, {
+            algorithms: ['HS256'],
+        })
+    },
     parseBasicToken(token){
         return Buffer
             .from(token, 'base64')
